31402d0b4a2ac33808b649c0b770fc87abb514d5
tiagovanderleidearruda@gmail.com <tiagovanderleidearruda@gmail.com>
2022-02-16 22:49:49 -0300

    fix: ListLength adjusted to implement recursion

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,12 @@
 defmodule ListLength do
   def call(list) do
-    list |> Enum.count()
+    count(list, 0)
   end
+
+  defp count([_head | tail], acc) do
+    acc = acc + 1
+    count(tail, acc)
+  end
+
+  defp count([], acc), do: acc
 end
